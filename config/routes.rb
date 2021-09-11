947f50779f2c6a53d439996e2ada73e0febedc1d
kosuke-w <s1lv3rt1psh4rk@gmail.com>
2021-09-11 13:34:05 +0000

    [Add]各コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'homes#top'
-  
+  resources :items
+  resources :coordinates
+  resources :posts, except: [:edit, :update] do
+    resources :post_comments, only: [:creste, :destroy]
+  end
+
+
 end
